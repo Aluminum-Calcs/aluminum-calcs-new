14ff81b7cf28f6bbbfadcf1ec30215622b111c95
Ehi Ejakhian <ehiejakhian@gmail.com>
2026-09-15 13:06:50 +0100

    fixed sign in header problems

--- src/components/ScrollToTop.jsx
+++ src/components/ScrollToTop.jsx
@@ -1,11 +1,21 @@
-import { useEffect } from 'react';
+import { useContext, useEffect } from 'react';
 import { useLocation } from 'react-router';
+import { PageContext } from '../context/PageContext';
 
 export default function ScrollToTop() {
   const { pathname } = useLocation();
+  const { setPreferences } = useContext(PageContext);
 
   useEffect(() => {
     window.scrollTo({ top: 0, left: 0, behavior: 'auto' });
+  }, [pathname]);
+
+  useEffect(() => {
+    setPreferences({
+      includeHeader: true,
+      includeAside: true,
+      includeFooter: true,
+    })
   }, [pathname]);
 
   return null;
